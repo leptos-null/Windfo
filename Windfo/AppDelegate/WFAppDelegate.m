5d2750a3985705d3126e7ea9db220deb9c21afa9
Leptos <leptos.0.null@gmail.com>
2019-09-22 23:58:26 -0700

    [Config] Minor project settings changes, improved Catalyst icon

--- Windfo/AppDelegate/WFAppDelegate.m
+++ Windfo/AppDelegate/WFAppDelegate.m
@@ -13,7 +13,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // I can't find it any where in the documentation, but unless this call is here,
     //   the menu bar is not what's in the storyboard
-    [UIMenuSystem.mainSystem setNeedsRebuild];
+    if (@available(iOS 13.0, *)) {
+        [UIMenuSystem.mainSystem setNeedsRebuild];
+    }
     return YES;
 }
+
 @end
